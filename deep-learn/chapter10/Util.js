f99a9dcfcd29094e1af21cb524b37da2ec87f59e
aung <aungpyaephyoe.dev@gmail.com>
2024-10-01 09:45:07 +0630

    Set and Map Standard Library

---
+++ deep-learn/chapter10/Util.js
@@ -0,0 +1,9 @@
+export default function api() {
+    console.log("Api from Util");
+}
+
+// only one default per module - file တစ်ဖိုင်မှာ default တစ်ခုပဲရှိလို့ရ
+
+export function another() {
+    console.log("Another is working");
+}
