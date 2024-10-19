b617a8405b2ad370009d5c732b92708807f2bb11
aung <aungpyaephyoe.dev@gmail.com>
2024-10-19 18:41:57 +0630

    Removed unnecessary files

---
+++ js-practice/chapter16/EventEmitter.js
@@ -0,0 +1,21 @@
+const EventEmitter = require("node:events");
+
+const eventEmitter = new EventEmitter();
+
+function executeLater()
+{
+    setTimeout(() => {
+        console.log("execute later done");
+        eventEmitter.emit('executeLaterDone', {
+            data: "Some Data"
+        });
+    },3000);
+}
+
+console.log("start");
+executeLater();
+
+eventEmitter.on("executeLaterDone", (event) => {
+    console.log("Run after execute later ", event);
+});
+console.log('done');
